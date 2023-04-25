c440fd592d316f988ae53d4033fa0f27c9b1a8ae
orngmrmld <127569177+orngmrmld@users.noreply.github.com>
2023-04-24 21:08:00 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// JS
+// Grab movies to choose from
+// Grab google info of that movie
+// Use google info to say where to watch (theater or stream)
+// Movie card information 
+// Use google maps api to find nearest location of theater
+// Update Movie card
+// Grab restaurants to choose from
+// Grab info on restaurant 
+// Use google maps api to find nearest location of restaurant
+// Randomizer
