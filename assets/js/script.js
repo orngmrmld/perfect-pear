6771c695312be965da1a4c31d4b3786a62b9912d
sarahm <minutemin@gmail.com>
2023-04-28 00:57:37 -0700

    add to html and css and java basic layout and function of filter buttons, clean up some html and css also.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,16 +10,24 @@
 // Use google maps api to find nearest location of restaurant
 // Randomizer
 
-var infocard = document.querySelector(".info-card");
+function filters() {
+  let filterBtn = $(".filterBtn");
+  filterBtn.on("click", function(event) {
+      event.preventDefault();
+      alert("hello world"); // just using this alert as a placeholder for the actual function we will be creating
+  });
+}
+filters();
+
 
 function initMap() {
-    var center = {lat: 51.507351, lng: -0.127758};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 10,
-      center: center
-    });
-    var marker = new google.maps.Marker({
-      position: center,
-      map: map
-    });
-  }
+  var center = {lat: 51.507351, lng: -0.127758};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: center
+  });
+  var marker = new google.maps.Marker({
+    position: center,
+    map: map
+  });
+}
