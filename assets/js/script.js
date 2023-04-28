28c0dcc39980f2df4ff250d98a4885fda23b9753
Gustavo <diazgus411@gmail.com>
2023-04-27 21:49:42 -0700

    sometimes working movie generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,28 @@
 // Grab info on restaurant 
 // Use google maps api to find nearest location of restaurant
 // Randomizer
+function getRandomMovie(event) {
+    // Generate a random movie ID
+    event.preventDefault();
+    var randomID = Math.floor(Math.random() * 100);
+    console.log(randomID)
+    // Make a request to the OMDB API with the random ID
+    fetch('http://www.omdbapi.com/?i=tt' + randomID + '&apikey=84310bbd')
+      .then(response => response.json())
+      .then(data => {
+        // Extract the movie information we need
+        var movieName = data.Title;
+        var movieRating = data.imdbRating;
+        var moviePoster = data.Poster;
+        // Display the movie information on the HTML page
+        var movieInfoDiv = document.querySelector('.movie-card');
+        movieInfoDiv.innerHTML = `
+          <h2>${movieName}</h2>
+          <p>Rating: ${movieRating}</p>
+          <img src="${moviePoster}">
+        `;
+      })
+      .catch(error => console.log(error));
+  }
+  
+  document.querySelector('.random').addEventListener('click', getRandomMovie);
