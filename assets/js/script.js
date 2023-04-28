f0f8d448b75bb50e2190bdca624c2267ecb916d2
Vagabond <marcusjjreyes@gmail.com>
2023-04-27 19:28:08 -0700

    added google map to website

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,17 @@
 // Grab info on restaurant 
 // Use google maps api to find nearest location of restaurant
 // Randomizer
+
+var infocard = document.querySelector(".info-card");
+
+function initMap() {
+    var center = {lat: 51.507351, lng: -0.127758};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: center
+    });
+    var marker = new google.maps.Marker({
+      position: center,
+      map: map
+    });
+  }
