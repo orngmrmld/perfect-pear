9af7afbab65b78dc85d0028a918a06b609074b73
sarahm <minutemin@gmail.com>
2023-05-03 23:15:30 -0700

    text white, hide movie card and center when shown, text for snacks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-const posterAPI = "http://img.omdbapi.com/?apikey=84310bbd&";
-const movieAPI = "http://www.omdbapi.com/?apikey=84310bbd&i=";
+const posterAPI = "https://img.omdbapi.com/?apikey=84310bbd&";
+const movieAPI = "https://www.omdbapi.com/?apikey=84310bbd&i=";
 const apiKey = "84310bbd";
 const movieIDs = ["tt0076759", "tt0080684", "tt0086190", "tt2488496", "tt0120915", "tt0121766", "tt0121765", "tt3748528", "tt2527336", "tt2527338"];
 // Get HTML elements
@@ -7,61 +7,66 @@ const movieCard = document.querySelector(".movie-card");
 
 //hide the movie filters until zipcode is entered
 $(".filters").hide();
-// $(".hide").hide();
+$(".hide").hide();
+
+
 
 //initiates the map on webpage
-function initMap() {
-  var center = { lat: 51.507351, lng: -0.127758 };
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 10,
-    center: center
-  });
-  var marker = new google.maps.Marker({
-    position: center,
-    map: map
-  });
-}
+// function initMap() {
+//   var center = { lat: 51.507351, lng: -0.127758 };
+//   var map = new google.maps.Map(document.getElementById('map'), {
+//     zoom: 10,
+//     center: center
+//   });
+//   var marker = new google.maps.Marker({
+//     position: center,
+//     map: map
+//   });
+// }
 
 //zipcode submit button saves to local storage and shows filter buttons, or displays error message
-$("#submit").on("click", function (event) {
-  event.preventDefault();
-  let zip = $("#zipCode").val();
-  localStorage.setItem("zip", zip);
+// $("#submit").on("click", function (event) {
+//   event.preventDefault();
+//   let zip = $("#zipCode").val();
+//   localStorage.setItem("zip", zip);
 
-  $("#zipCode").val("");
+//   $("#zipCode").val("");
 
-  if (zip) {
-    $(".zipDisplay").text("Zipcode: " + zip);
-    $(".filters").show()
-    $(".hideInput").hide();
-  } else {
-    $(".zipDisplay").text("Sorry, please enter a zipcode");
-    $(".filters").hide();
-  }
-});
+//   if (zip) {
+//     $(".zipDisplay").text("Zipcode: " + zip);
+//     $(".filters").show()
+//     $(".hideInput").hide();
+//   } else {
+//     $(".zipDisplay").text("Sorry, please enter a zipcode");
+//     $(".filters").hide();
+//   }
+// });
 
-function isUSAZipCode(str) 
-{
-  return /^\d{5}(-\d{4})?$/.test(str);
-}
+// function isUSAZipCode(str) 
+// {
+//   return /^\d{5}(-\d{4})?$/.test(str);
+// }
 
-function validateInput() 
-{
-  preventDefault();
-  console.log("validateInput");
-  let zipCode = document.getElementById("zipCode").value;
-  let message = "";
-  if (isUSAZipCode(zipCode)) 
-  {
-    message = "Valid Zip Code:";
-  } else {
-    message = "Invalid Zip Code";
-  }
-  document.getElementById("msg").innerHTML = message;
-}
+// function validateInput() 
+// {
+//   preventDefault();
+//   console.log("validateInput");
+//   let zipCode = document.getElementById("zipCode").value;
+//   let message = "";
+//   if (isUSAZipCode(zipCode)) 
+//   {
+//     message = "Valid Zip Code:";
+//   } else {
+//     message = "Invalid Zip Code";
+//   }
+//   document.getElementById("msg").innerHTML = message;
+// }
+
+
 //function for the movie random generator
 // Define function to generate random movie
 async function generateRandomMovie() {
+  console.log('genereateButton click')
   // Generate random movie ID from the provided movie IDs
   const randomID = movieIDs[Math.floor(Math.random() * movieIDs.length)];
   // Fetch movie details from API
@@ -73,15 +78,16 @@ async function generateRandomMovie() {
   const moviePoster = `${posterAPI}&i=${randomID}`;
   // Create movie card HTML
   const movieCardHTML = `
-  <div class="columns is-mobile">
+  <div class="content columns hide">
   
     <div class="column">
       <img src="${moviePoster}" alt="${movieTitle} poster">
     </div>
 
     <div class="column">
-      <h3>${movieTitle}</h3>
-      <h4>Rating: ${movieRating}</h4>
+      <h2 class="has-text-white">${movieTitle}</h2>
+      <h4 class="has-text-white">Rating: ${movieRating}</h4>
+      <h2 class="has-text-white">HERE IS YOUR SNACK MATCH ---><h2>
     </div>
 
     <div class="column">
@@ -91,16 +97,18 @@ async function generateRandomMovie() {
   `;
   // Update movie card with HTML
   movieCard.innerHTML = movieCardHTML;
+  $(".filters").show();
   $(".hide").show();
 }
 
 // Generator for random filter click functions, pairs with random restaurant locator
-$(".randomBtn").on("click", function (event) {
+$("#randomBtn").on("click", function (event) {
   event.preventDefault();
+  console.log('random button click')
   localStorage.setItem("filter", "random");
   generateRandomMovie();
-  $(".randomBtn").text("Press to pick another movie!");
-  $(".hideComment").hide();
+  $("#randomBtn").text("Press to pick another movie!");
+  // $(".hideComment").hide();
   //restaurantRandom():
 })
 
